4061923176b47ac10436a25cbac033ae9d8a23b5
pashamakhilkumarreddy <pashamakhilkumarreddy@gmail.com>
2023-07-25 19:07:21 +0530

    update config

--- src/config/development.js
+++ src/config/development.js
@@ -5,7 +5,7 @@ const _ENV_ = process.env
 const config = {
   server: {
     HOST: _ENV_.HOST || '127.0.0.1',
-    PORT: _ENV_.PORT || 3000
+    PORT: _ENV_.PORT || 5000
   }
 }
 
--- src/config/production.js
+++ src/config/production.js
@@ -5,7 +5,7 @@ const _ENV_ = process.env
 const config = {
   server: {
     HOST: _ENV_.HOST || '0.0.0.0',
-    PORT: _ENV_.PORT || 3000
+    PORT: _ENV_.PORT || 5000
   }
 }
 
